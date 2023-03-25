d6704d5ea6cf0d89a7ec1a973340df220adca824
Vyacheslav <slava-russia@mail.ru>
2023-03-25 19:03:51 +0700

    Task 1.3 done

---
+++ L1/hw1_3.java
@@ -0,0 +1,34 @@
+// Реализовать простой калькулятор (+ - / *)
+// Ввод числа ->
+// Ввод знака ->
+// Ввод числа ->
+
+package L1;
+
+import java.util.Scanner;
+
+public class hw1_3 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter first number ");
+        int firstNum = inp.nextInt();
+        String sign = inp.next();
+        int secondNum = inp.nextInt();
+        switch (sign) {
+            case "+":
+                System.out.println(firstNum + secondNum);
+                break;
+            case "-":
+                System.out.println(firstNum - secondNum);
+                break;
+            case "*":
+                System.out.println(firstNum * secondNum);
+                break;
+            case "/":
+                System.out.println((double) firstNum / secondNum);
+                break;
+            default:
+                System.out.println("Error!");
+        }
+    }
+}
